961af120401e64d6a2ec9766729f2d78807d10b2
S2 <s@ireoo.com>
2019-02-12 16:38:27 +0800

    格式化程序

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ func main() {
 		fmt.Printf(`API-Core version: %s`, version)
 		return
 	}
+
 	// var result bson.M
 	// err := mongo.FindOne("api", "user", bson.M{"username": "18551410359"}, bson.M{"_id": 0}, &result)
 	// if err != nil {
